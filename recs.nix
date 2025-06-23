1b96e19edc5cfe3e22bd2b49b8767d11797b7606
spaceoden <26421479+spaceoden@users.noreply.github.com>
2025-06-23 16:50:29 +0200

    Update recs.nix to removed settings now in hardened-ish.nix

--- recs.nix
+++ recs.nix
@@ -1,33 +1,12 @@
 { lib, pkgs, ... }:
 {
 
-  #automatically deploy updates
-  system.autoUpgrade = {
-    enable = true;
-    randomizedDelaySec = "600"; #adds 0-10 minutes to trigger time to stagger updates
-    operation = "boot";
-  };
-
   #io-scheduler
   hardware.block.defaultScheduler = "kyber";
   hardware.block.defaultSchedulerRotational = "bfq";
   #this takes precedence over above defaultScheduler
   hardware.block.scheduler = {
     "mmcblk[0-9]*" = "bfq";
-  };
-
-  #clean up old deployments
-  nix.optimise.automatic = true;
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than 7d";
-  };
-
-  #randomize MAC
-  networking.networkmanager = {
-    ethernet.macAddress = "stable";
-    wifi.macAddress = "random";
   };
 
   #lavd autopower scheduler
@@ -37,6 +16,5 @@
     package = pkgs.scx.rustscheds;
     scheduler = "scx_lavd";
   };
-
 
 }
