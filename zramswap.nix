701be8dbcbe437341ad895fef3aa0a111b06ef47
spaceoden <26421479+spaceoden@users.noreply.github.com>
2025-08-04 00:16:57 +0000

    add zramswap.nix

---
+++ zramswap.nix
@@ -0,0 +1,16 @@
+{
+
+  zramSwap = {
+    enable = true;
+    algorithm = "lz4";
+    memoryPercent = 63;
+  };
+
+  boot.kernel.sysctl = {
+    "vm.swappiness" = 180;
+    "vm.watermark_boost_factor" = 0;
+    "vm.watermark_scale_factor" = 125;
+    "vm.page-cluster" = 0;
+  };
+
+}
